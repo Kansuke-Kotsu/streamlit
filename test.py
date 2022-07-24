511242e54af120971bf5da5f74c679961bb56d31
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2022-07-24 22:51:55 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,118 @@
+import streamlit as st
+from bs4 import BeautifulSoup
+from turtle import title
+import requests
+from urllib.request import urlopen
+import numpy as np
+from random import random
+import random
+import cv2
+import io
+from PIL import Image
+from email.mime import image
+import base64
+import streamlit.components.v1 as stc
+
+
+
+st.title("Battle Spirits デッキシミュレーリョン")
+URL = "https://club.battlespirits.com/bsclub/mydeck/decksrc/202207/11657463961882_20220710.html"
+
+# HTMLを解析
+html = urlopen(URL)
+data = html.read()
+html = data.decode('utf-8')
+soup = BeautifulSoup(html, 'html.parser')
+
+st.write(URL)
+
+#変数定義
+head_url = "https://club.battlespirits.com"
+images = []
+card_name = []
+card_counts =[]
+counts =[]
+deck =[]
+n = 0  # 契約スピリット判定用
+
+# 解析したHTMLから任意の部分のみを抽出
+images = soup.select("main > section > div > ul > li")
+card_name = soup.select("main > section > div > ul > li")
+
+for i in range(len(images)):
+  images[i] = images[i].find("img")
+  card_name[i] = card_name[i].find("img")
+  if images[i].get("src").endswith(".jpg"): # imgタグ内の.jpgであるsrcタグを取得
+    images[i] = images[i].get("src")
+  if card_name[i].get("alt"):
+    card_name[i] = card_name[i].get("alt")
+
+# 各カードの枚数を取得
+print("---------デッキ情報---------")
+card_counts = soup.find_all('p', class_='cardCount')
+for i in range(len(card_counts)):
+  if card_counts[i].string == "1枚":
+    counts.append(1)
+  elif card_counts[i].string == "2枚":
+    counts.append(2)
+  else:
+    counts.append(3)
+
+#契約スピリットがいる場合
+for i in range(len(images)):
+  if card_name[i] == "相棒竜グロウ" or card_name[i] == "相棒狼ランポ" or card_name[i] == "相棒騎士バット" or card_name[i] == "相棒機スターク" or card_name[i] == "相棒鳥フェニル" or card_name[i] == "相棒鮫シャック":
+    keiyaku = images[i]
+    counts[i] = counts[i] - 1
+    n =1
+    break
+
+# デッキ作成
+for i in range(len(counts)):
+  for j in range(counts[i]):
+    deck.append(head_url + images[i])
+
+# シャッフル
+shuffle_deck = deck
+random.shuffle(shuffle_deck)
+if n == 1:
+  deck.insert(0, head_url + keiyaku)
+
+#画像表示
+def show_cards(nums):
+    col= st.columns(nums)
+    for i in range(nums):
+        with col[i]:
+            st.image(shuffle_deck[0])
+            shuffle_deck.pop(0)
+
+def draw_botton():
+    if st.button("1枚ドロー", key=2):
+        show_cards(nums=1)
+
+    if st.button("2枚ドロー", key=3): 
+        show_cards(nums=2)
+  
+    if st.button("3枚ドロー", key=4): 
+        show_cards(nums=3)
+
+    draw_botton()
+
+
+def main():
+    show_cards(nums=5)
+    #text = st.text_input("表示したい単語を入力してください")
+    if 'count' not in st.session_state: 
+        st.session_state.count = 0 #countがsession_stateに追加されていない場合，0で初期化
+
+    increment = st.button('Increment')
+    if increment:
+        st.session_state.count += 1 #値の更新
+    draw_botton()
+        
+
+
+if __name__ == '__main__':
+    main()
+    
+
+
